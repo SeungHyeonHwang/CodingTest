e055e9d77e43b53df2cba22f10eaf281be3e3746
Seunghyeon Hwang <shh0456@gmail.com>
2020-07-01 16:15:19 +0900

    modified

--- programmers/python/brute-force_search/숫자야구.py
+++ programmers/python/brute-force_search/숫자야구.py
@@ -8,65 +8,61 @@ Created on Tue Jun 30 12:29:25 2020
 
 # In[] : https://programmers.co.kr/learn/courses/30/lessons/42841
 # 숫자야구
+# 다시풀
 
-
-# def dfs(i) : 
-#     n,s,b = baseball
-    
-    
-
-import itertools 
-def solution(baseball):
-    answer = 0 
-    
-    baseball = sorted(baseball, key=lambda x : (x[1], x[2]), reverse=True)
-    
-    for n,s,b in baseball : 
-        
-    
-    return answer
-
-
-baseball = [[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]	
-print(solution(baseball))
-
-# In[]
-
-
-def st_B(given, chosen):
-    st = 0
-    B = 0
-    chosen_dif = []
-    given_dif = []
-    for i in range(3):
-        if given[i] == chosen[i]:
-            st += 1
-        else:
-            given_dif.append(given[i])
-            chosen_dif.append(chosen[i])
-    for num in chosen_dif:
-        if num in given_dif:
-            B += 1
-    return st, B
 
 import itertools
 
-def solution(baseball):
-    first = list(itertools.permutations([1,2,3,4,5,6,7,8,9], 3))
-    second = []
-    for each in baseball:
-        given = [int(i) for i in str(each[0])]
-        print(given)
-        stb = (each[1], each[2])
-        for chosen in first:
-            if st_B(given, chosen) == stb:
-                second.append(chosen)
-        first = second
-        second = []
-    return len(first)
+def check(inning, cand) : 
+    st, ba = 0,0
+    
+    b_cand_inning = []
+    b_cand = []
+    
+    for i in range(3) : 
+        if inning[i] == cand[i] :
+            st+=1
+        else :
+            b_cand_inning.append(inning[i])
+            b_cand.append(cand[i])
+            
+    for num in b_cand_inning :
+        if num in cand : 
+            ba +=1
+        
+    return st, ba
+
+def solution(baseball) : 
+    answer = 0
+    
+    candidate = list(itertools.permutations([i for i in range(1,10)], 3))
+    
+    
+    for inning in baseball : 
+        cand_list = []
+
+        for cand in candidate : 
+        
+            pred = [int(i) for i in str(inning[0])]    
+            if check(pred, cand) == (inning[1], inning[2]) : 
+                cand_list.append(cand)
+        candidate = cand_list
+    answer = len(candidate)
+    return answer 
+
+
+
+
 
 baseball = [[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]	
 print(solution(baseball))
 
+
+
 # In[]
+
+
+
+
+
 
